fa4677137abd0097aa1fb3a11cdb8647ab354644
Jeffrey Yao <jeffreyyao@Jeffreys-MacBook-Pro-2.local>
2020-05-06 08:35:18 -0700

    final

---
+++ java.js
@@ -0,0 +1,55 @@
+console.log(hello);
+var a = document.getElementById('projects1');
+var b = document.getElementById('schedule1');
+var c = document.getElementById('projects1');
+var d = document.getElementById('resources1');
+
+function linkText(a) {
+  document.getElementById('projects1').style.color = "transparent";
+  document.getElementById('resources1').style.color = "transparent";
+  document.getElementById('schedule1').style.color = "transparent";
+  document.getElementById('coursedes1').style.color = "black";
+  document.getElementById('a').style.color = "rgb(237,103,219)";
+  document.getElementById('b').style = "pink";
+  document.getElementById('c').style = "pink";
+  document.getElementById('d').style = "pink";
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+}
+
+function linkText1(b) {
+  document.getElementById('projects1').style.color = "transparent";
+  document.getElementById('resources1').style.color = "transparent";
+  document.getElementById('schedule1').style.color = "black";
+  document.getElementById('coursedes1').style.color = "transparent";
+  document.getElementById('a').style = "pink";
+  document.getElementById('b').style.color = "rgb(237,103,219)";
+  document.getElementById('c').style = "pink";
+  document.getElementById('d').style = "pink";
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+}
+function linkText2(c) {
+  document.getElementById('projects1').style.color = "black";
+  document.getElementById('resources1').style.color = "transparent";
+  document.getElementById('schedule1').style.color = "transparent";
+  document.getElementById('coursedes1').style.color = "transparent";
+  document.getElementById('a').style = "pink";
+  document.getElementById('b').style = "pink";
+  document.getElementById('c').style.color = "rgb(237,103,219)";
+  document.getElementById('d').style = "pink";
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+}
+function linkText3(d) {
+  document.getElementById('projects1').style.color = "transparent";
+  document.getElementById('resources1').style.color = "black";
+  document.getElementById('schedule1').style.color = "transparent";
+  document.getElementById('coursedes1').style.color = "transparent";
+  document.getElementById('a').style = "pink";
+  document.getElementById('b').style = "pink";
+  document.getElementById('c').style = "pink";
+  document.getElementById('d').style.color = "rgb(237,103,219)";
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+}
